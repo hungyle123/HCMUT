99e99c0901cdcab127da82239d61babd88808a17
Nguyễn Tiến Hưng <138371452+hungyle123@users.noreply.github.com>
2024-05-28 23:13:38 +0700

    Delete HK231/Computer - Architecture/LAB_Programming_Fundamental/LAB_Programming_Fundamental directory

--- HK231/Computer - Architecture/LAB_Programming_Fundamental/LAB_Programming_Fundamental/Week1/Prelab/ClassString_Question1.cpp
+++
@@ -1,7 +0,0 @@
-void cutString(string s, int index){
-    int a= s.length();
-    if (index < a){
-      string s1=s.substr(index);
-      cout<< s1;}
-    
-}
--- HK231/Computer - Architecture/LAB_Programming_Fundamental/LAB_Programming_Fundamental/Week1/Prelab/Multi-dimensional-array_Question2.cpp
+++
@@ -1,7 +0,0 @@
-int diagonalProduct(int arr[][1000], int row, int col) {
-    int k =1;
-    for (int i = 0; i < row;i++){
-        k = k*arr[i][i];
-    }
-    return k;
-}
